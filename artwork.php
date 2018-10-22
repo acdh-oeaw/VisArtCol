f1ce2e922be6ec79e840b21237dda1d15e594877
Asil Çetin <25176039+asilcetin@users.noreply.github.com>
2018-10-22 16:12:59 +0200

    app groundwork

---
+++ artwork.php
@@ -0,0 +1,201 @@
+<?php
+  include("functions.php");
+  if (isset($_GET["artworkID"])) {
+      $artworkID = $_GET["artworkID"];
+  } else {
+      $artworkID = 0;
+  }
+  $artwork = getArtworkData($artworkID);
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Fundament WP - Example HTML Page">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <title><?php echo $artwork["title"]; ?></title>
+    <link rel="stylesheet" id="fundament-styles"  href="fundament/css/fundament.min.css" type="text/css"/>
+    <!-- app.css -->
+    <link rel="stylesheet" id="app-styles"  href="app.css" type="text/css"/>
+    <!-- Plotly.js -->
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+  </head>
+  <body class="page">
+    <div class="hfeed site" id="page">
+      <!-- ******************* The Navbar Area ******************* -->
+      <div class="wrapper-fluid wrapper-navbar sticky-navbar" id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+        <a class="skip-link screen-reader-text sr-only" href="#content">Skip to content</a>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container" >
+            <!-- Your site title as branding in the menu -->
+            <a href="/" class="navbar-brand custom-logo-link" rel="home" itemprop="url"><img src="images/visartcol-logo.svg" class="img-fluid" alt="VisArtCol Logo" itemprop="logo" /></a><!-- end custom logo -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+              <!-- Your menu goes here -->
+              <ul id="main-menu" class="navbar-nav">
+                <li class="nav-item active"><a title="Home" href="/" class="nav-link">Home</a></li>
+                <li class="nav-item dropdown">
+                  <a title="Dropdown" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle">Dropdown <span class="caret"></span></a>
+                  <ul class=" dropdown-menu" role="menu">
+                    <li class="nav-item dropdown-submenu">
+                      <a title="Level 1" href="#" class="nav-link">Level 1</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item"><a title="Documentation" href="#" class="nav-link">Documentation</a></li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0 navbar-search-form" method="get" action="/" role="search">
+                <input class="form-control navbar-search" id="s" name="s" type="text" placeholder="Search" value="" autocomplete="off">
+                <button type="submit" class="navbar-search-icon">
+                  <i data-feather="search"></i>
+                </button>
+              </form>
+            </div>
+            <!-- .collapse navbar-collapse -->
+          </div>
+          <!-- .container -->
+        </nav>
+        <!-- .site-navigation -->
+      </div>
+      <!-- .wrapper-navbar end -->
+
+
+
+
+
+
+
+
+<div class="wrapper" id="page-wrapper">
+  <div class="container" id="content" tabindex="-1">
+    <div class="row">
+      <div class="col-md-12 col-lg-12 content-area" id="primary">
+        <main class="site-main" id="main" role="main">
+          <article>
+            <header class="entry-header text-center">
+              <h2><?php echo $artwork["title"].", ".$artwork["year"]; ?></h2>
+              <h4><?php echo $artwork["artist"]; ?></h3>
+            </header>
+            <div class="row">
+              <div class="col-md-4 col-lg-4 artwork-visual-container">
+                <img src="<?php echo $artwork["imgURL"]; ?>" alt="<?php echo $artwork["title"]; ?>" />
+                <div class="artwork-palette">
+                <?php
+                foreach ($artwork["rgbPalette"] as $i => $rgbPalette) {
+                  // Show max 5 colors
+                  if ($i < 5) {
+                    $colorRatio = $rgbPalette[3] * 100;
+                    $hexColor = sprintf("#%02x%02x%02x", $rgbPalette[0], $rgbPalette[1], $rgbPalette[2]);
+                    echo '
+                      <div class="palette-swatch">
+                        <div class="palette-swatch-color" style="background:rgb('.$rgbPalette[0].','.$rgbPalette[1].','.$rgbPalette[2].');"></div>
+                        <div class="palette-swatch-ratio">'.$colorRatio.'%</div>
+                        <div class="palette-swatch-hex">'.$hexColor.'</div>
+                      </div>
+                    ';
+                  }
+                }
+                ?>
+                </div>
+              </div>
+              <div class="col-md-8 col-lg-8 artwork-plot-container">
+                <div id="#artwork-plot"></div>
+                <?php echo getArtwork3DPlot($artworkID); ?>
+              </div>
+            </div><!-- .row -->
+          </article>
+        </main>
+      </div>
+    </div><!-- .row -->
+  </div><!-- Container end -->
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div class="wrapper fundament-default-footer" id="wrapper-footer-full">
+        <div class="container" id="footer-full-content" tabindex="-1">
+          <div class="footer-separator">
+            <i data-feather="message-circle"></i> CONTACT
+          </div>
+          <div class="row">
+            <div class="footer-widget col-lg-1 col-md-2 col-sm-2 col-xs-6 col-3">
+              <div class="textwidget custom-html-widget">
+                <a href="/"><img src="https://fundament.acdh.oeaw.ac.at/common-assets/images/acdh_logo.svg" class="image" alt="ACDH Logo" style="max-width: 100%; height: auto;" title="ACDH Logo"></a>
+              </div>
+            </div>
+            <!-- .footer-widget -->
+            <div class="footer-widget col-lg-4 col-md-4 col-sm-6 col-9">
+              <div class="textwidget custom-html-widget">
+                <p>
+                  ACDH-ÖAW
+                  <br>
+                  Austrian Centre for Digital Humanities
+                  <br>
+                  Austrian Academy of Sciences
+                </p>
+                <p>
+                  Sonnenfelsgasse 19,
+                  <br>
+                  1010 Vienna
+                </p>
+                <p>
+                  T: +43 1 51581-2200
+                  <br>
+                  E: <a href="mailto:acdh@oeaw.ac.at">acdh@oeaw.ac.at</a>
+                </p>
+              </div>
+            </div>
+            <!-- .footer-widget -->
+            <div class="footer-widget col-lg-3 col-md-4 col-sm-4 ml-auto">
+              <div class="textwidget custom-html-widget">
+                <h6>HELPDESK</h6>
+                <p>ACDH runs a helpdesk offering advice for questions related to various digital humanities topics.</p>
+                <p>
+                  <a class="helpdesk-button" href="mailto:acdh-tech@oeaw.ac.at">ASK US!</a>
+                </p>
+              </div>
+            </div>
+            <!-- .footer-widget -->
+          </div>
+        </div>
+      </div>
+      <!-- #wrapper-footer-full -->
+      <div class="footer-imprint-bar" id="wrapper-footer-secondary" style="text-align:center; padding:0.4rem 0; font-size: 0.9rem;">
+        © Copyright OEAW | <a href="https://www.oeaw.ac.at/die-oeaw/impressum/">Impressum/Imprint</a>
+      </div>
+    </div>
+    <!-- #page we need this extra closing tag here -->
+    <script type="text/javascript" src="fundament/vendor/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="fundament/js/fundament.min.js"></script>
+  </body>
+</html>
---
+++ functions.php
@@ -0,0 +1,226 @@
+<?php
+
+function getArtworkData($artworkID) {
+  $file = file_get_contents("data/metadata.json");
+  $data = json_decode($file, true);
+  if ($artworkID == "all") {
+    return $data["artworks"];
+  }
+  foreach($data["artworks"] as $artwork) {
+    if ($artwork["id"] == $artworkID) {
+      return $artwork;
+    }
+  }
+}
+
+//Add new artwork meta data
+function addArtworkData($formData) {
+  $file = file_get_contents("data/metadata.json");
+  $data = json_decode($file, true);
+  $latestId = $data["latestId"];
+  $newId = $latestId + 1;
+
+  savePixelColors( $_POST['url'], $newId );
+  $paletteColors = getHistogramPalette('data/'.$newId.'.csv');
+
+  //Check if artwork with this url exists
+  foreach($data["artworks"] as $artwork) {
+    if ($artwork["imgURL"] == $formData["url"]) {
+      return "Artwork exists";
+    }
+  }
+
+  $data["latestId"] = $newId;
+  array_push($data["artworks"], array('id' => $newId, 'artist' => $formData["artist"], 'title' => $formData["title"], 'year' => $formData["year"], 'imgURL' => $formData["url"], 'medium' => $formData["medium"], 'dimensions' => $formData["dimensions"], 'currentLocation' => $formData["location"], 'rgbPalette' => $paletteColors));
+  $new_data = json_encode($data);
+  if (file_put_contents("data/metadata.json", $new_data) === false){
+    unset($new_data);
+    return false;
+  } else {
+    unset($new_data);
+    return "Artwork metadata added.";
+  }
+}
+
+function getArtwork3DPlot($artworkID) {
+
+  return "
+    <script>
+    Plotly.d3.csv('data/".$artworkID.".csv', function(err, rows){
+
+      function unpack(rows, key) {
+        return rows.map(function(row) {
+          return row[key]; 
+        });
+      }
+
+      function dotcolor(rows) {
+        return rows.map(function(row) { 
+          return 'rgb('+row['r']+','+row['g']+','+row['b']+')'; 
+        });
+      }
+
+      var data = {
+        x: unpack(rows, 'r'), y: unpack(rows, 'g'), z: unpack(rows, 'b'),
+        mode: 'markers',
+        marker: {
+          size: 2,
+          color: dotcolor(rows),
+          line: {
+            color: 'rgb(90, 90, 90)',
+            width: 0.1
+          }
+        },
+        type: 'scatter3d'
+      };
+
+      var layout = {
+        margin: { l:0, r:0, b: 0, t: 0 },
+        scene:{ 
+          camera: {
+            center: { x: 0, y: 0, z: 0 }, 
+            eye: { x: -1, y: 1.75, z: 0 }, 
+            up: { x: 0, y: 1, z: 0 }
+          },
+          xaxis: {
+            title: 'Red',
+            tickfont: {
+              color:'red',
+            },
+          },
+          yaxis: {
+            title: 'Green',
+            tickfont: {
+              color:'green',
+            },
+          },
+          zaxis: {
+            title: 'Blue',
+            tickfont: {
+              color:'blue',
+            },
+          }
+        },
+      };
+
+      Plotly.newPlot('#artwork-plot', [data], layout);
+
+    });
+    </script>
+  ";
+
+}
+
+function savePixelColors($img, $newId) {
+
+  $MAX_WIDTH    = 150;
+  $MAX_HEIGHT   = 150;
+
+  $size = GetImageSize($img);
+  $scale = 1;
+  if ($size[0]>0) {
+    $scale = min($MAX_WIDTH/$size[0], $MAX_HEIGHT/$size[1]);
+    if ($scale < 1) {
+      $width = floor($scale*$size[0]);
+      $height = floor($scale*$size[1]);
+    } else {
+      $width = $size[0];
+      $height = $size[1];
+    }
+  }
+  $image_resized = imagecreatetruecolor($width, $height);
+  if ($size[2] == 1) $image_orig = imagecreatefromgif($img);
+  if ($size[2] == 2) $image_orig = imagecreatefromjpeg($img);
+  if ($size[2] == 3) $image_orig = imagecreatefrompng($img);
+
+  imagecopyresampled($image_resized, $image_orig, 0, 0, 0, 0, $width, $height, $size[0], $size[1]);
+  $im = $image_resized;
+
+  $imgWidth = imagesx($im);
+  $imgHeight = imagesy($im);
+  $colorData = array();
+  $total_pixel_count = 0;
+  
+  for ($y=0; $y < $imgHeight; $y++) {
+    for ($x=0; $x < $imgWidth; $x++) {
+
+      $colorIndex = imagecolorat($im, $x, $y);
+      $colorRGB = imagecolorsforindex($im, $colorIndex);
+      $colorData[] = [$colorRGB['red'],$colorRGB['green'],$colorRGB['blue']];
+
+    }
+  }
+
+  $csv = "r,g,b\n";//Column headers
+  foreach ($colorData as $record){
+      $csv.= $record[0].','.$record[1].','.$record[2]."\n"; //Append data to csv
+  }
+
+  $csv_handler = fopen('data/'.$newId.'.csv','w');
+  fwrite ($csv_handler,$csv);
+  fclose ($csv_handler);
+}
+
+function getHistogramPalette($csv) {
+
+  $bucketPerDimension = 2;
+  $bucketMax = 256;
+  $bucketSize = $bucketMax / $bucketPerDimension;
+
+  $csvFile = file($csv);
+  $buckets = array();
+  $allPixelsSize = 0;
+  foreach ($csvFile as $line => $pixel) {
+    if ($line > 0) {
+      $allPixelsSize++;
+      $pixel = str_getcsv($pixel);
+      $pixelBucketIndex = getPixelBucketIndex($pixel, $bucketSize);
+      if (!array_key_exists($pixelBucketIndex, $buckets)) {
+        $buckets[$pixelBucketIndex] = array();
+      }
+      array_push($buckets[$pixelBucketIndex], $pixel);
+    }
+  }
+  $averageColors = getBucketsAverageColor($buckets);
+
+  usort($averageColors, function($a, $b) {
+    return $b[3] - $a[3];
+  });
+
+  foreach ($averageColors as $i => $averageColor) {
+    $averageColors[$i][3] = number_format(($averageColor[3]/$allPixelsSize), 4);
+  }
+  return $averageColors;
+}
+
+function getPixelBucketIndex($pixel, $bucketSize) {
+  $redBucket = floor($pixel[0] / $bucketSize);
+  $greenBucket = floor($pixel[1] / $bucketSize);
+  $blueBucket = floor($pixel[2] / $bucketSize);
+  $pixelBucketIndex = $redBucket.":".$greenBucket.":".$blueBucket;
+  return $pixelBucketIndex;
+}
+
+function getBucketsAverageColor($buckets) {
+  $averageColors = array();
+  foreach ($buckets as $bucketIndex => $bucket) {
+    $bucketSize = 0;
+    $redSum = 0;
+    $blueSum = 0;
+    $greenSum = 0;
+    foreach ($bucket as $key => $pixel) {
+      $bucketSize++;
+      $redSum += $pixel[0];
+      $blueSum += $pixel[1];
+      $greenSum += $pixel[2];
+    }
+    $bucketAverageRed = round($redSum / $bucketSize);
+    $bucketAverageBlue = round($blueSum / $bucketSize);
+    $bucketAverageGreen = round($greenSum / $bucketSize);
+    array_push($averageColors, [$bucketAverageRed,$bucketAverageBlue,$bucketAverageGreen,$bucketSize]);
+  }
+  return $averageColors;
+}
+
+
+?>
