1a7cd38d21f72b0b13fa4e374f7dda05cfca5d0b
Asil Çetin <25176039+asilcetin@users.noreply.github.com>
2018-11-24 15:30:12 +0100

    default config params

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 /* This file defines global variables for the application */
-define("HEADER_TEMPLATE", "custom-templates/app-header.php");
-define("FOOTER_TEMPLATE", "custom-templates/app-footer.php");
+define("HEADER_TEMPLATE", "header.php");
+define("FOOTER_TEMPLATE", "footer.php");
 
 ?>
